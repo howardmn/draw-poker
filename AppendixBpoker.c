4343c5001926c8e307f144e44c5b016df86fc0dd
Howard Moon <44125785+howardmn@users.noreply.github.com>
2018-10-31 21:06:29 -0700

    Create AppendixBpoker.c

---
+++ AppendixBpoker.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// Two constants defined for determinging whether hands are flushes or straights.
+
+#define FALSE 0
+#define TRUE 1
